361041f0fde698ac7193022539e2c0ce8bad18c5
Artur Wróblewski <kryp-alkora@wp.pl>
2019-09-05 22:31:56 +0200

    CharacterControl-Camera

--- Assets/StoneSmash/Prototype/Scripts/CameraDriver.cs
+++ Assets/StoneSmash/Prototype/Scripts/CameraDriver.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class CameraDriver : MonoBehaviour
 {
+    /// <summary>
+    /// Target is what the camera looks at.
+    /// </summary>
+
+    [SerializeField] private Transform observationTarget;
+    [SerializeField] private float verticalOffset;
+    [SerializeField] private float followOffset;
+    [SerializeField] private float followSpeed = 2.0f;
+    [SerializeField] private bool lookAtTarget;
+
+    private Vector3 targetPosition;
+    private Vector3 targetDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +24,27 @@ public class CameraDriver : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+        Look();
+    }
+    private void Move()
+    {
+        // Relative to the world
+        //targetPosition = new Vector3(observationTarget.transform.position.x, verticalOffset, observationTarget.transform.position.z - followOffset);
+
+        // Relative to the actor / observation Target
+        targetPosition = new Vector3(observationTarget.transform.position.x, verticalOffset + observationTarget.transform.position.y, observationTarget.transform.position.z - followOffset);
+        targetDirection = targetPosition - transform.position;
+        transform.position += targetDirection * Time.deltaTime * followSpeed;
+            
+    }
+    private void Look()
+    {
+        if (lookAtTarget)
+        {
+            transform.LookAt(observationTarget);
+        }
     }
 }
